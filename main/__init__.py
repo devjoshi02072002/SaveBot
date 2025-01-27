a138067b2a40a19e26b0fc25c814700d9ed80149
devjoshi02072002 <65372615+devjoshi02072002@users.noreply.github.com>
2025-01-27 15:17:07 +0530

    __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("27944955", default=None, cast=int)
+API_HASH = config("2e045584d56a1c086d4091a1b02eb36f", default=None)
+BOT_TOKEN = config("8186269324:AAH9ZzoGaT1dHiMaajRZVRc9irFTnNejrXE", default=None)
+SESSION = config("BQFfhFEAbVgLQW0DWwdpv7QAiPH1LPB3IqIT3Hf3IpzfMq6j1CtbJluP6Q83vhGge31E06YMYkBO0LrZiavlNAh_eoZVYXg4yr4WT9xRlk5TXodeQSGbDJpJ52Go-u8Matuhv_R9NPkTZZC7Hr25JD5RBThyffPlmwXpsJj-SF4cg7uNF51kzEQQp62Syurm_HU0wIQZBBExPJ9NlyDnjpEADbNabNVk__ppKgJCsf-j4NGGneuBa18dwYfAJMnrSRsfHlWCRbOOyoImF74aIevJ3HTOVqi-kEMjj1BMPeV2-bO_Smq1ulzdW3BsKXOa6kb4qb4Ltl1T4YYqoiCdETug8XZfugAAAABRtBDvAA ", default=None)
+FORCESUB = config("hellopriye", default=None)
+AUTH = config("1370755311", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
